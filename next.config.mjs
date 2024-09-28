87c8b7cf9a60be429c95ba1b7032bdf8c63bd647
IDerevyansky <igor.derevyansky@gmail.com>
2024-09-28 12:09:27 -0700

    add rout ru

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          // Basic redirect
+          {
+            source: '/',
+            destination: '/ru',
+            permanent: true,
+          }
+        ]
+      },
+};
+
+
+
+
 
 export default nextConfig;
