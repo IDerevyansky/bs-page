7f1a98b61c8eccc99a78638dcdfa35aa2d0b81d9
IDerevyansky <igor.derevyansky@gmail.com>
2024-09-28 13:24:11 -0700

    remov old font

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={ramaraja.className}>
+      <body>
         {children}
       </body>
     </html>
