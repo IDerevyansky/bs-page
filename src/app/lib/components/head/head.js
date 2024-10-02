c070f96c07e882c4845a99d25e1426736fe46bb5
IDerevyansky <igor.derevyansky@gmail.com>
2024-10-01 23:33:11 -0700

    fix social icon

--- src/app/lib/components/head/head.js
+++ src/app/lib/components/head/head.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import './head.css';
 
-import fb from '@/app/lib/img/fb.svg';
-import inst from '@/app/lib/img/in.svg';
-import tg from '@/app/lib/img/tg.svg';
+import fb from '@/app/lib/img/fb.png';
+import inst from '@/app/lib/img/in.png';
+import tg from '@/app/lib/img/tg.png';
 
 export default function Head(){
     return(
@@ -19,34 +19,34 @@ export default function Head(){
                 </div>
                 <div className='socialIcon'>
 
-                    <Link href='#'>
+                    <Link href='#' target='blank_'>
                         <Image
                         src={fb}
-                        width='32px'
-                        height='32px'
+                        width='32'
+                        height='32'
                         alt='facebook'
                         />
                     </Link>
 
-                    <Link href='#'>
+                    <Link href='#' target='blank_'>
                         <Image
                         src={inst}
-                        width='32px'
-                        height='32px'
+                        width='32'
+                        height='32'
                         alt='instagram'
                         />
                     </Link>
 
-                    <Link href='#'>
+                    <Link href='#' target='blank_'>
                         <Image
                         src={tg}
-                        width='32px'
-                        height='32px'
+                        width='32'
+                        height='32'
                         alt='telegram'
                         />
                     </Link>
 
-                    <Link href='#' className='appointment'>
+                    <Link href='https://wa.me/18182987426?text=Здравствуйте%20Наталья,%20я%20хочу%20записаться%20к%20вам%20на%20прием.' target='blank_' className='appointment'>
                         Appointment
                     </Link>
 
@@ -63,4 +63,3 @@ export default function Head(){
         </div>
     )
 }
-//fb-in-tg-wt
