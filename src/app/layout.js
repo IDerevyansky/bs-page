4a32717542024218fced86afc36080be26acce6d
IDerevyansky <17588032+IDerevyansky@users.noreply.github.com>
2025-06-02 02:46:44 -0700

    add manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,14 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="ru">
+
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#484E45" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
+
+
       <body>
         {children}
       </body>
